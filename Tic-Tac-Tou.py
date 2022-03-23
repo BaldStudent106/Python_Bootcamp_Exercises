d56964b9e287a03f4b4ef0620006426e9f4afd0d
BaldStudent106 <jiasoonong148@gmail.com>
2022-03-24 02:13:50 +0800

    Created display function

--- Tic-Tac-Tou.py
+++ Tic-Tac-Tou.py
@@ -2,6 +2,39 @@
 This is my simple Tic Tac Tou game created for my Udemy Bootcamp Milestone Project
 '''
 
+#fucntion to display the tic tac tou table
 def display():
     num="O"
+    print("\n")
+    print("\t \t|\t \t|")
     print(f"\t{num}\t|\t{num}\t|\t{num}\t")
+    print("_"*50)
+    print("\t \t|\t \t|")
+    print(f"\t{num}\t|\t{num}\t|\t{num}\t")
+    print("_"*50)
+    print("\t \t|\t \t|")
+    print(f"\t{num}\t|\t{num}\t|\t{num}\t")
+    print("\n")
+
+def first():
+    white=int
+    black=int
+    player=[white,black]
+    for i in range(len(player)):
+        while(True):
+            player[i]=input("White will enter a number")
+            if player[i].isdigit()==False:
+                print("The number entered is not a digit. Please try again")
+            else:
+                break
+    if player[0]>player[1]:
+        return 1
+    else:
+        return 2
+
+while(True):
+    game_count=1
+    if game_count==1:
+        print("Welcome to this Tic Tac Tou game!")
+    first_result=first()
+    display()
