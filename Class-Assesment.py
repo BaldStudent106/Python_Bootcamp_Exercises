28b39e36ba154854c5439c185034ab08bdb88a8f
BaldStudent106 <jiasoonong148@gmail.com>
2022-03-26 02:11:04 +0800

    Created Class-Assesment.py

---
+++ Class-Assesment.py
@@ -0,0 +1,14 @@
+'''
+This is my exercise for OOP in Python
+'''
+
+#Question 1: Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
+
+class Line():
+    def __init__(self,c1,c2):
+        self.x1,self.x2=c1
+        self.y2,self.y1=c2
+    
+    def distance():
+        return()
+
