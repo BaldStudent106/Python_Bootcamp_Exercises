eb42f366736b66e462d9b38a4c34f5e1a1ca6a36
BaldStudent106 <jiasoonong148@gmail.com>
2022-03-26 02:27:32 +0800

    Finished Question 1

--- Class-Assesment.py
+++ Class-Assesment.py
@@ -4,11 +4,35 @@ This is my exercise for OOP in Python
 
 #Question 1: Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
 
+
+#define the object Line
 class Line():
+
+    #def line init method
     def __init__(self,c1,c2):
         self.x1,self.x2=c1
         self.y2,self.y1=c2
-    
-    def distance():
-        return()
+
+    #def Line distance method
+    def distance(self):
+        return((self.x2-self.x1)**2 + (self.y2-self.y1)**2)**0.5
+
+    #def Line slope method
+    def slope(self):
+        return(self.y2-self.y1)/(self.x2-self.x1)
+
+
+#define the value or coordinates
+coor1=(5,6)
+coor2=(9,1)
+
+#Created a Line object
+line_example=Line(coor1,coor2)
+
+#Call distance method
+print(line_example.distance)
+
+#Call slope method
+print(line_example.slope(coor1,coor2))
+
 
